dbce60586c27042094a14cf4bea3bd189f17b991
theEast17 <nagarpoorv2001@gmail.com>
2024-06-03 18:46:35 +0530

    add dashboard layout

---
+++ src/layout/DashBoardLayout.tsx
@@ -0,0 +1,143 @@
+import { Button } from "@/components/ui/button";
+import { Link, Outlet } from "react-router-dom";
+import {
+  Bell,
+  CircleUser,
+  Home,
+  Menu,
+  Package,
+  Package2,
+  Search,
+} from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const DashboardLayout = () => {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link
+              to="/dashboard"
+              className="flex items-center gap-2 font-semibold"
+            >
+              <Package2 className="h-6 w-6" />
+              <span>Coder's Book</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                to="/dashboard"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 hover:bg-slate-100 transition-all"
+              >
+                <Home className="h-4 w-4" />
+                Home
+              </Link>
+
+              <Link
+                to="/dashboard/books"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:bg-slate-100"
+              >
+                <Package className="h-4 w-4" />
+                Books{" "}
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  to="/dashboard"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <Package2 className="h-6 w-6" />
+                  <span className="sr-only">Coder's Book</span>
+                </Link>
+                <Link
+                  to="/dashboard"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2  hover:bg-slate-100"
+                >
+                  <Home className="h-5 w-5" />
+                  Home
+                </Link>
+
+                <Link
+                  to="/dashboard/books"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2  hover:bg-slate-100"
+                >
+                  <Package className="h-5 w-5" />
+                  Books
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+
+          <div className="w-full flex-1">
+            <form>
+              <div className="relative">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search products..."
+                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                />
+              </div>
+            </form>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./layout/DashBoardLayout";
+import BookPage from "./pages/BookPage";
+
 
 export const router=createBrowserRouter([
     {
-        path:'/',
-        element:<HomePage/>
+        path:'/dashboard',
+        element:<DashboardLayout/>,
+        children:[
+            {
+                path:'/dashboard',
+                element:<HomePage/>
+            },
+            {
+                path:'books',
+                element:<BookPage/>
+            },
+
+        ]
     },
     {
         path:'/login',
